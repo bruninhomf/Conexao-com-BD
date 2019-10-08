ecc8834116f70eeb877f5d7f23af83c1df037277
Bruno Firmiano <bruninhomf@msn.com>
2019-10-07 21:52:33 -0300

    Criação de conexão com banco de dados

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Celke</title>
+    </head>
+    <body>
+        <?php
+        require './Conn.php';
+        
+        $conn = new Conn();
+        $conn->getConn();
+        
+        var_dump($conn);
+        ?>
+    </body>
+</html>
