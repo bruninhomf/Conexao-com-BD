dd8222184da2425142bd3876b3b19e86d392476a
Bruno Firmiano <bruninhomf@msn.com>
2019-10-08 00:32:01 -0300

    Identação do código e insersão de comentário.

--- Conn.php
+++ Conn.php
@@ -1,18 +1,15 @@
 <?php
-
 /**
  * Script de conexão
  *
  * @copyright (c) 2019, Bruno Firmiano
  */
 class Conn {
-    
     public static $Host = "localhost";
     public static $User = "root";
     public static $Pass = "";
     public static $Dbname = "celke";
     private static $Connect = null;
-    
     private static function Conectar() {
         try {
             if(self::$Connect == null):
@@ -21,12 +18,12 @@ class Conn {
         } catch (Exception $ex) {
             echo 'Mensagem: ' . $ex->getMessage();
             die;
-        }       
+        }
         return self::$Connect;
     }
-    
+
     public function getConn() {
         return self::Conectar();
     }
-    
+
 }
